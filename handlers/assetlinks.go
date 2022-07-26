beeb98c934c04ab5a15fbb69c5c2cad6f21a9c5d
Javier Coronel Orovio <orovium@pop-os.localdomain>
2022-07-26 15:01:19 +0200

    Change app info

--- handlers/assetlinks.go
+++ handlers/assetlinks.go
@@ -19,8 +19,8 @@ func (handler *AssetLinksHandler) SendManifest(c *gin.Context) {
 			"relation": []string{"delegate_permission/common.handle_all_urls"},
 			"target": gin.H{
 				"namespace":                "android_app",
-				"package_name":             "com.example",
-				"sha256_cert_fingerprints": []string{"14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5"},
+				"package_name":             "com.romanillos.pack",
+				"sha256_cert_fingerprints": []string{"AD:F4:9F:F3:B4:EB:23:50:24:3E:F0:AA:8A:E3:8F:EE:1A:79:39:33:36:28:16:4B:63:D4:9A:F3:DF:8F:72:5A"},
 			},
 		}})
 }
